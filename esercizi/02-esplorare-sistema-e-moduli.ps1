3b8fce44951e97a3a570e9c626e170a73512eda7
K3rnelP4nic <78502942+jekopepp@users.noreply.github.com>
2026-09-02 23:10:33 +0200

    Basic test
    some basic test in powershell

--- esercizi/02-esplorare-sistema-e-moduli.ps1
+++ esercizi/02-esplorare-sistema-e-moduli.ps1
@@ -13,42 +13,47 @@ function Show-Section {
     Write-Host "`n=== $Title ===" -ForegroundColor Cyan
 }
 
+#permette di vedere la data e l'oriro con il formato richiesto
 Show-Section 'Data e fuso orario'
 Get-TimeZone | Select-Object Id, DisplayName, BaseUtcOffset
 Get-Date -Format 'dddd dd/MM/yyyy HH:mm'
 
+#permette di vedere il profilo di rete con gli oggetti selezionati dopo "select-object"
 Show-Section 'Profilo di rete'
 Get-NetConnectionProfile |
     Select-Object Name, InterfaceAlias, NetworkCategory, IPv4Connectivity, IPv6Connectivity
 
+#fa vedere la versione di powershell installata sul pc
 Show-Section 'Versione di PowerShell'
-$PSVersionTable | Select-Object PSVersion, PSEdition, OS
+$PSVersionTable.PSversion
 
+#fa vedere le cartelle e i percorsi dove powershell fa la ricerca dei moduli
 Show-Section 'Percorsi in cui PowerShell cerca i moduli'
 $env:PSModulePath -split [IO.Path]::PathSeparator
 
+#fa vedere quali moduli sono caricati nella sessione corrente in uso
 Show-Section 'Moduli caricati nella sessione'
 Get-Module | Sort-Object Name | Select-Object Name, Version, ModuleType
 
+#fa vedere la lista completa di tutti i moduli presente sul pc
 Show-Section 'Moduli disponibili sul computer'
 Get-Module -ListAvailable |
     Sort-Object Name |
     Select-Object Name, Version, ModuleType, Path
 
+#usato come test per vedere quali copmandi ha il modulo (in questo caso applocker)
 Show-Section 'Comandi del modulo AppLocker (se disponibile)'
 Get-Command -Module AppLocker -ErrorAction SilentlyContinue |
     Select-Object CommandType, Name, Version
 
-<#
-Per provare AppLocker, prima carica il modulo:
 
+#Per provare AppLocker, prima carica il modulo:
     Import-Module AppLocker
     Get-Command -Module AppLocker
 
-La policy locale, se configurata, si legge con:
 
+#La policy locale, se configurata, si legge con:
     Get-AppLockerPolicy -Local
 
-Il comando può richiedere l'esecuzione di PowerShell come amministratore.
-#>
+#Il comando può richiedere l'esecuzione di PowerShell come amministratore.
 
